8841b4efe3e73567f513fe0f286298aaa4e452ad
arbind@bridgeft.com <arbind@bridgeft.com>
2021-06-13 12:16:38 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Header from "./components/Header"
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
+  const [tasks, setTasks] = useState(
+
+    [{
+      "id": 1,
+      "text": "Email Client",
+      "day": "June 13th at 12:15pm",
+      "reminder": true
+    },
+    {
+      "id": 2,
+      "text": "Meet Dan",
+      "day": "June 20th at 5:00pm",
+      "reminder": true
+    }, 
+    { 
+      "id": 3,
+      "text": "Grocery Shopping",
+      "day": "June 21th at 7:10pm",
+      "reminder": true
+    }, 
+]
+)
+
+
+// Delete Tasks 
+
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+// Toggle Reminder
+
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task)=> task.id === id ? 
+  {...task, reminder:!task.reminder}: 
+  task))
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <AddTask />
+      {tasks.length > 0 ? 
+      (<Tasks tasks ={tasks} onDelete={deleteTask} 
+        onToggle={toggleReminder} />
+        ):(
+        'No Tasks To Show'
+      )}
+
+      
     </div>
   );
 }
---
+++ src/components/Tasks.js
@@ -0,0 +1,18 @@
+import SingleTask from "./SingleTask"
+
+const Tasks = ({tasks , onDelete, onToggle}) => {
+    
+    return (
+        <>
+          {tasks.map((task)=> (
+          <SingleTask 
+          key={task.id} 
+          task={task} 
+          onDelete={onDelete}
+          onToggle={onToggle}> </SingleTask>
+          ))}  
+        </>
+    )
+}
+
+export default Tasks
